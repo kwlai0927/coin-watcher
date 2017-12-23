3276a954f1bd9def3d384410c896a163dde4f144
Lavend K. Mi <lavend@richhk.net>
2017-12-23 17:32:04 +0800

    practice hbs

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
+
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 });
@@ -15,6 +16,23 @@ hbs.registerHelper('screamIt', (text) => {
 
 var app = express();
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err){
+      console.log('Unable to append to server.log.');
+    }
+  })
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
@@ -27,6 +45,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Project Page'
   });
 });
 
