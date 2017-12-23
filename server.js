2d78233cb20f5c5f2c13efd8ac3beffefe571add
Lavend K. Mi <lavend@richhk.net>
2017-12-23 17:10:32 +0800

    Templating

--- server.js
+++ server.js
@@ -1,29 +1,41 @@
-const express = require("express");
-const hbs = require("hbs");
-var app = express();
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+var app = express();
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name:  'Lavend',
-    likes: [
-      'Biking'
-      , 'Shopping'
-    ]
-  })
+  res.render('home.hbs', {
+    pageTitle: 'Home Page'
+    , welcomeMessage: 'Welcome to my website'
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about.hbs');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
 app.get('/bad', (req, res)=> {
-  res.send({errorMassag});
+  res.send({
+    errorMassag: 'Unable to handle request'
+  });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000')
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
 });
